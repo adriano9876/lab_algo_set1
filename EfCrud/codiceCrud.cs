65950636f3ee6cd9727fb632d8b2ac44fdeeceed
val-git01 <val.acc001@outlook.it>
2025-10-27 07:20:56 +0100

    Update codiceCrud.cs

--- EfCrud/codiceCrud.cs
+++ EfCrud/codiceCrud.cs
@@ -7,12 +7,69 @@
 
 public async Task<ActionResult<IEnumerable<PalyerDto> GetPlayers()
 {
+  var players = _context.Players.AsNoTracking ().Select (p => new PlayerDto
+                                                         {
 
-
-
-
-
-
-
-
+                                                         });
+  return await players.ToListAsinc();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
